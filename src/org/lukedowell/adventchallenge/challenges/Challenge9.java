478de6bc81ac74c97c39e3c68206a877566d5a26
Luke Dowell <lukedowell@gmail.com>
2015-12-09 14:50:06 -0600

    Begins laying base for GA

--- src/org/lukedowell/adventchallenge/challenges/Challenge9.java
+++ src/org/lukedowell/adventchallenge/challenges/Challenge9.java
@@ -4,63 +4,95 @@ import org.lukedowell.adventchallenge.ChallengeProcessor;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by ldowell on 12/9/15.
  */
 public class Challenge9 extends ChallengeProcessor {
 
-    private Collection<City> cities;
-
     public Challenge9() {
         super(new File("res/nine.txt"));
-        cities = new ArrayList<>();
     }
 
     @Override
     public void process() throws Exception {
-        getInputStrings().forEach(route -> {
+        getInputStrings().forEach(tour -> {
+            String[] tour_data = tour.split(" ");
 
-            // 0 city name, 2 destination name, 4 distance
-            String[] route_data = route.split(" ");
-
-            // Pull our city objects
-            City origin = findOrCreateCity(route_data[0]);
-            City dest = findOrCreateCity(route_data[2]);
-
-            // Parse the distance
-            int distance = Integer.parseInt(route_data[4]);
         });
     }
 
     /**
-     * Tries to find a city with a given name. If
-     * none can be found, a city is created, added to our list,
-     * and returned
-     */
-    private City findOrCreateCity(String name) {
-        for(City city : cities) {
-            if(city.name.equalsIgnoreCase(name)) {
-                return city;
-            }
-        }
-        City city = new City("name");
-        cities.add(city);
-        return city;
-    }
-
-    /**
-     * A city is just a container for a bunch of vectors
+     * Representation of an endpoint in our
+     * traveling salesman problem
      */
     private class City {
 
-        private Collection<Vector> route_distances;
+        /** The name of the city */
         private String name;
+
+        /** A list of tours branching out from this city */
+        private List<Tour> tours;
 
         public City(String name) {
             this.name = name;
+            tours = new ArrayList<>();
+        }
+
+        public void addTour(Tour t) {
+            this.tours.add(t);
+        }
+
+        public int getDistanceToCity(City city) {
+            for(Tour t : tours) {
+                if(t.getDestination().getName().equalsIgnoreCase(city.getName())) {
+                    return t.getDistance();
+                }
+            }
+            return 9999;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<Tour> getTours() {
+            return tours;
+        }
+    }
+
+    /**
+     * Represents a path between two cities.
+     */
+    private class Tour {
+
+        /** The 'from' city */
+        private City origin;
+
+        /** The 'to' city */
+        private City destination;
+
+        /** The distance between the origin and the destination */
+        private int distance;
+
+        public Tour(City origin, City destination, int distance) {
+            this.origin = origin;
+            this.destination = destination;
+            this.distance = distance;
+        }
+
+        public City getOrigin() {
+            return origin;
+        }
+
+        public City getDestination() {
+            return destination;
+        }
+
+        public int getDistance() {
+            return distance;
         }
     }
 }
